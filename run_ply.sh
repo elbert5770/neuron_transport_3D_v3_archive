c6f0c5a69e237031d9b4dd498454e69512eeb912
Don Elbert <40698678+elbert5770@users.noreply.github.com>
2025-04-02 10:17:57 -0700

    Update run_ply.sh
    Updated file name

--- run_ply.sh
+++ run_ply.sh
@@ -4,7 +4,7 @@
 for i in {1..8}
 do
     echo "Running PlyToJLD2 with argument $i"
-    mpiexec -n 40 julia --depwarn=yes --project ./Neuron_transport_programs/PlyToJLD2_3D_1.0a.jl $i
+    mpiexec -n 40 julia --depwarn=yes --project ./Neuron_transport_programs/PlyToJLD2_3D_3.0a.jl $i
     
     # Check if the program executed successfully
     if [ $? -eq 0 ]; then
